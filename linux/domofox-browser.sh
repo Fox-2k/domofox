2595ce5fc0d0b6487117868d349ea75cc9781aed
Yoann <yoann@denkisekai.me>
2022-08-06 10:29:21 +0200

    domofox-browser service & script fix

--- linux/domofox-browser.sh
+++ linux/domofox-browser.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
+# Rotate screen touch (Raspberry pi 4 with official Raspberry pi screen)
+DISPLAY=:0 sudo xinput --set-prop 'raspberrypi-ts' 'Coordinate Transformation Matrix' 0 1 0 -1 0 1 0 0 1
+
 # Enable screen saver (after 60sec)
 xset dpms 0 0 60 
-
-# Start Vnc Server
-logger -t startbrowser 'Start VNC Server...'
-x11vnc -bg -forever -quiet -passwdfile vncpasswd > /dev/null
 
 # Hide mouse
 unclutter -idle 0.1 &
 
 # Window manager
 x-window-manager &
+
+# Start Vnc Server
+#logger -t startbrowser 'Start VNC Server...'
+#x11vnc -bg -forever -passwdfile /home/yoann/domofox/linux/vncpasswd > /dev/null
 
 echo "Wait 2 secs ..."
 sleep 2
@@ -22,5 +25,5 @@ sleep 2
 # rm /home/pi/.config/chromium/Singleton*
 
 # Launch Chromium
-su -c "chromium-browser --proxy-auto-detect --disk-cache-dir=/dev/null --media-cache-dir=/dev/null --disable-notifications --no-default-browser-check --disable-pinch --allow-insecure-localhost --disable-extensions --disable-background-networking --disable-device-discovery-notifications --disable-component-update --disable-component-extensions-with-background-pages --disable-default-apps --remote-debugging-port=9222 --user-data-dir=remote-profile --kiosk $1" -s /bin/sh pi
+su -c "chromium-browser --proxy-auto-detect --disk-cache-dir=/dev/null --media-cache-dir=/dev/null --disable-notifications --no-default-browser-check --disable-pinch --allow-insecure-localhost --disable-extensions --disable-background-networking --disable-device-discovery-notifications --disable-component-update --disable-component-extensions-with-background-pages --disable-default-apps --remote-debugging-port=9222 --user-data-dir=remote-profile --kiosk $1" -s /bin/sh yoann
 #su -c "chromium-browser --proxy-auto-detect --disk-cache-dir=/dev/null --media-cache-dir=/dev/null --disable-notifications --no-default-browser-check --start-fullscreen --allow-insecure-localhost --disable-extensions --disable-background-networking --disable-device-discovery-notifications --disable-component-update --disable-component-extensions-with-background-pages --disable-default-apps --enable-logging --v=1 --remote-debugging-port=9222 --user-data-dir=remote-profile --app=$1" -s /bin/sh pi
