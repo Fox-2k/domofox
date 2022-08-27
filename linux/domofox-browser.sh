94659b8e164c9b019d04e1e78261a5328ed98e17
Yoann <yoann@denkisekai.me>
2022-08-27 10:28:23 +0200

    Change screen saver activation delay to 5min

--- linux/domofox-browser.sh
+++ linux/domofox-browser.sh
@@ -3,8 +3,8 @@
 # Uncomment to rotate screen touch (Raspberry pi 4 with official Raspberry pi screen)
 # DISPLAY=:0 sudo xinput --set-prop 'raspberrypi-ts' 'Coordinate Transformation Matrix' 0 1 0 -1 0 1 0 0 1
 
-# Enable screen saver (after 60sec)
-xset dpms 0 0 60 
+# Enable screen saver (after 5min)
+xset dpms 0 0 300 
 
 # Hide mouse
 unclutter -idle 0.1 &
