e342770d3de011ae9b380bdee18953f4b9a59a93
Yoann <yvermaut@dickson-constant.com>
2023-05-17 17:57:57 +0200

    Adding components

--- react/src/components/block.tsx
+++ react/src/components/block.tsx
@@ -1,30 +1,47 @@
 import { SxProps } from "@mui/material"
 import Box from "@mui/material/Box"
+import Button, { ButtonProps } from '@mui/material/Button'
+import { styled } from '@mui/material/styles'
+import { purple } from '@mui/material/colors';
+import { MouseEventHandler } from "react"
 
 interface BlockProps {
     sx?: SxProps,
     height?: number
     width?: number,
     children: React.ReactNode,
-    icon: React.ReactNode
+    icon: React.ReactNode,
+    onClick?: MouseEventHandler<HTMLButtonElement>
 }
 
-export default function Block({ sx, height, width, children, icon } : BlockProps) {
-    return (
-        <Box
-            sx={{
-                position: 'relative',
-                bgcolor: '#00000055',
-                borderTopLeftRadius: 20,
-                borderBottomRightRadius: 20,
-                p: 2,
-                mx: '10px',
-                height: height ?? 180,
-                width: width ?? 200,
-                color: 'text.primary',
-                ...sx
-            }}
-        >
+const BlockButton = styled(Button)<ButtonProps>(({theme}) => ({
+    background: "repeating-linear-gradient(0deg, #00000088, #00000055 20%, #00000055 80%, #00000088)",
+    '&:hover': {
+        backgroundColor: "#000000AA",
+    },
+}))
+
+export default function Block({ sx, height, width, children, icon, onClick } : BlockProps) {
+    const customSx: SxProps = {
+        position: 'relative',
+        bgcolor: onClick ? "transparent" : '#00000055',
+        borderTopLeftRadius: 20,
+        borderBottomRightRadius: 20,
+        p: 2,
+        mx: '10px',
+        height: height ?? 180,
+        width: width ?? 200,
+        color: 'text.primary',
+        ":hover": "inherit",
+        ...sx
+    }
+
+    const container = (content: React.ReactNode) => {
+        return onClick ? <BlockButton sx={customSx} onClick={onClick}>{content}</BlockButton> : <Box sx={customSx}>{content}</Box>
+    }
+
+    return container(
+        <>
             <Box
                 sx={{
                     position: 'absolute',
@@ -43,6 +60,6 @@ export default function Block({ sx, height, width, children, icon } : BlockProps
                 {icon}
             </Box>
             {children}
-        </Box>   
+        </>
     )
 }
--- react/src/components/valueBlock.tsx
+++ react/src/components/valueBlock.tsx
@@ -4,6 +4,13 @@ import Dialog from '@mui/material/Dialog';
 import DialogTitle from '@mui/material/DialogTitle';
 import Box from "@mui/material/Box"
 import Button from '@mui/material/Button';
+import Paper from '@mui/material/Paper';
+import KeyboardDoubleArrowLeftIcon from '@mui/icons-material/KeyboardDoubleArrowLeft';
+import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight';
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import CancelIcon from '@mui/icons-material/Cancel';
 
 import Block from "./block";
 
@@ -34,15 +41,21 @@ function EditValueDialog({ onClose, initialValue, open} : EditValueDialogProps) 
 
     return (
         <Dialog onClose={handleClose} open={open}>
-            <DialogTitle sx={{ textAlign: "center", fontSize: 50, p:0 }}>{value}</DialogTitle>
+            <Paper >
+                <DialogTitle sx={{ textAlign: "center", fontSize: 50, p:0 }}>{value}</DialogTitle>
+            </Paper>
             <Box sx={{ display: 'flex' }} >
-                <Button sx={{ m: 2, minWidth: 150, minHeight: 50 }} variant="contained">--</Button>
-                <Button sx={{ m: 2, minWidth: 150, minHeight: 50 }} variant="contained">++</Button>
+                <Button sx={{ m: 2, minWidth: 150, minHeight: 50 }} variant="contained"><KeyboardDoubleArrowLeftIcon /></Button>
+                <Button sx={{ m: 2, minWidth: 150, minHeight: 50 }} variant="contained"><KeyboardDoubleArrowRightIcon /></Button>
             </Box>
             <Box sx={{ display: 'flex' }} >
-                <Button sx={{ m: 2, minWidth: 150, minHeight: 50 }} variant="contained">-</Button>
-                <Button sx={{ m: 2, minWidth: 150, minHeight: 50 }} variant="contained">+</Button>
+                <Button sx={{ m: 2, minWidth: 150, minHeight: 50 }} variant="contained"><KeyboardArrowLeftIcon /></Button>
+                <Button sx={{ m: 2, minWidth: 150, minHeight: 50 }} variant="contained"><KeyboardArrowRightIcon /></Button>
             </Box>
+            <Paper elevation={-3} sx={{ display: 'flex' }} >
+                <Button sx={{ m: 2, minWidth: 150, minHeight: 50, backgroundColor: "secondary.main" }} variant="contained"><CancelIcon /></Button>
+                <Button sx={{ m: 2, minWidth: 150, minHeight: 50, backgroundColor: "success.main" }} variant="contained"><CheckCircleIcon /></Button>
+            </Paper>
         </Dialog>
     )
 }
@@ -66,29 +79,28 @@ export default function ValueBlock({ icon, value, unit, editable } : valueBlockP
 
     return (
         <>
-            <Button onClick={handleClickOpen}>
-                <Block 
-                    icon={icon}
+            <Block 
+                icon={icon}
+                sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    fontSize: 56,
+                    cursor: editable ? "pointer" : "inherit",
+                }}
+                onClick={editable ? handleClickOpen : undefined }
+            >
+                <Box
                     sx={{
                         display: "flex",
-                        flexDirection: "column",
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                        fontSize: 56,
-                        cursor: editable ? "pointer" : "inherit",
+                        alignItems: 'baseline',
                     }}
                 >
-                    <Box
-                        sx={{
-                            display: "flex",
-                            alignItems: 'baseline',
-                        }}
-                    >
-                        <div>{value}</div>
-                        <div style={{fontSize: 30}}>{unit ?? ""}</div>
-                    </Box>
-                </Block>
-            </Button>
+                    <div>{value}</div>
+                    <div style={{fontSize: 30}}>{unit ?? ""}</div>
+                </Box>
+            </Block>
             <EditValueDialog initialValue={value} open={openDialog} onClose={handleClose}/>
         </>
     )
